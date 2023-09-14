06b47a0ffdff14e45b2a22511d4060ffcd8a4845
Othniel Agera <otagera@gmail.com>
2023-09-14 12:42:06 +0100

    FIxed User controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,80 +1,98 @@
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import mongoose from "mongoose";
+import mongoose, { ObjectId } from "mongoose";
 
-import { get, post, bodyValidator, controller } from "../../decorators/index";
-import { User } from "./users.interface";
+import { get, controller } from "../../decorators/index";
+import { IUser } from "./users.interface";
 import { UsersService } from "./users.service";
+import { HttpStatus, StandardAPIResponseFn } from "@lib/utils";
 
-const UserModel = mongoose.model<User>("User");
+const UserModel = mongoose.model<IUser>("User");
 
 @controller("/users")
-export class AuthController {
-	constructor(private userService: UsersService) {}
+export class UserController {
+	private static _usersService: UsersService = new UsersService();
 
 	@get("/exist/:username")
 	async checkUserExistence(req: Request, res: Response) {
 		const { username } = req.params;
-		let data = { status: false };
 		try {
-			const user: User = await UserModel.findOne({
+			const users: IUser[] = await UserController._usersService.find({
 				username: username,
 			});
-			if (!user) {
-				return res.statusJson(404, { data: data });
+			if (users.length < 1) {
+				return res.statusJson(
+					HttpStatus.NOT_FOUND,
+					StandardAPIResponseFn(
+						`User with username: ${username} doesn't exist!`,
+						false
+					)
+				);
 			}
-			data.status = true;
-			return res.statusJson(200, { data: data });
+			return res.statusJson(
+				HttpStatus.OK,
+				StandardAPIResponseFn(
+					`User with username: ${username} does exist!`,
+					true
+				)
+			);
 		} catch (error) {
-			console.log(error);
-			data["error"] = error;
-			return res.statusJson(500, { data: data });
+			return res.statusJson(
+				HttpStatus.INTERNAL_SERVER_ERROR,
+				StandardAPIResponseFn("Something went wrong!", false, error)
+			);
 		}
 	}
 	@get("/users")
 	async getAllUsers(req: Request, res: Response) {
-		let data: { status: boolean; users: User[] } = {
-			status: false,
-			users: [],
-		};
 		try {
-			const users = await UserModel.find({});
+			const users: IUser[] = await UserController._usersService.find({});
 			if (users.length === 0) {
-				return res.statusJson(404, { data: data });
+				return res.statusJson(
+					HttpStatus.NOT_FOUND,
+					StandardAPIResponseFn(`No user in the DB`, false)
+				);
 			}
-			data.status = true;
-			data.users = users;
-			return res.statusJson(200, { data: data });
+			return res.statusJson(
+				HttpStatus.OK,
+				StandardAPIResponseFn(`Users in DB`, true, { users })
+			);
 		} catch (error) {
-			console.log(error);
-			data["error"] = error;
-			return res.statusJson(500, { data: data });
+			return res.statusJson(
+				HttpStatus.INTERNAL_SERVER_ERROR,
+				StandardAPIResponseFn("Something went wrong!", false, error)
+			);
 		}
 	}
 	@get("/users/:id")
 	async getUser(req: Request, res: Response) {
 		const { id } = req.params;
-		let data: { status: boolean; message?: String; user?: User } = {
+		let data: { status: boolean; message?: String; user?: IUser } = {
 			status: false,
 		};
 		try {
 			if (!mongoose.Types.ObjectId.isValid(id)) {
-				data.message = "Invalid object id";
-				return res.statusJson(400, { data: data });
+				return res.statusJson(
+					HttpStatus.NOT_ACCEPTABLE,
+					StandardAPIResponseFn(`Invalid ObjectId: ${id}`, false)
+				);
 			}
-			const user = await UserModel.findOne({ id });
+			const objectId = new mongoose.Schema.Types.ObjectId(id);
+			const user = await UserController._usersService.findOne(objectId);
 			if (!user) {
-				data.message = "User not found";
-				return res.statusJson(404, { data: data });
+				return res.statusJson(
+					HttpStatus.NOT_FOUND,
+					StandardAPIResponseFn(`User not found`, false)
+				);
 			}
-			data.status = true;
-			data.user = user;
-			return res.statusJson(200, { data: data });
+			return res.statusJson(
+				HttpStatus.OK,
+				StandardAPIResponseFn(`User fetched successfully`, true, { user })
+			);
 		} catch (error) {
-			console.log(error);
-			data["error"] = error;
-			return res.statusJson(500, { data: data });
+			return res.statusJson(
+				HttpStatus.INTERNAL_SERVER_ERROR,
+				StandardAPIResponseFn("Something went wrong!", false, error)
+			);
 		}
 	}
 }
